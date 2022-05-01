9be67bd1e31cc63b68114d07b0cf22146a5fe194
unknown <claudiosasuke@gmail.com>
2022-04-30 23:29:08 -0300

    menu-mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,28 @@ window.onscroll = function(){
     }else{
         menu.classList.remove('active')
     }
+}
+
+showMenu = (toogleId, navId) => {
+    const toggle = menuToggle = document.getElementById(toogleId)
+          navMenu = document.getElementById(navId)
+
+          if(toggle && nav){
+              toggle.addEventListener('click',()=>{
+                  nav.classList.toggle('active__menu')
+              })
+          }     
  }
+    
+showMenu('menu__toggle','nav')    
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    
+    // Remove menu mobile
+    const navMenu = document.getElementById('nav')
+    navMenu.classList.remove('active__menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
